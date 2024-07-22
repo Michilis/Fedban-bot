829e49349c4fff894634fc9f6bb7eba786bad107
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 20:44:58 +0200

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  telegramToken: process.env.TELEGRAM_TOKEN,
-  databaseUrl: process.env.DATABASE_URL,
-  logGroupId: process.env.LOG_GROUP_ID,
-};
