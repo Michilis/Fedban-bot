34c04a3a546b2d245793f822595e9fe360bf39c9
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 21:16:27 +0200

    Update messages.js

--- messages.js
+++ messages.js
@@ -1,61 +1,9 @@
-const messages = {
-  start: "Welcome to the Federation Bot! Use /help to see available commands.",
-  help: `
-Here are the available commands:
-
-- /start - Show the welcome message
-- /help - Show this help message
-- /newfed <name> - Create a new federation
-- /myfeds - List federations you own
-- /joinfed <id> - Join a federation
-- /leavefed <id> - Leave a federation
-- /fedinfo <id> - Get information about a federation
-- /fedadmins <id> - List federation admins
-- /fpromote <id> - Promote a federation admin
-- /fdemote <id> - Demote a federation admin
-- /fban <id> - Ban a user from a federation
-- /unfban <id> - Unban a user from a federation
-  `,
-
+module.exports = {
+  start: 'Welcome to the Federation Bot! Use /help to see available commands.',
   helpMenu: {
-    main: `
-Here are the available commands:
-
-- /start - Show the welcome message
-- /help - Show this help message
-- /newfed <name> - Create a new federation
-- /myfeds - List federations you own
-- /joinfed <id> - Join a federation
-- /leavefed <id> - Leave a federation
-- /fedinfo <id> - Get information about a federation
-- /fedadmins <id> - List federation admins
-- /fpromote <id> - Promote a federation admin
-- /fdemote <id> - Demote a federation admin
-- /fban <id> - Ban a user from a federation
-- /unfban <id> - Unban a user from a federation
-    `,
-    admin: `
-Admin Commands:
-- /fpromote <id> - Promote a federation admin
-- /fdemote <id> - Demote a federation admin
-- /fban <id> - Ban a user from a federation
-- /unfban <id> - Unban a user from a federation
-    `,
-    owner: `
-Owner Commands:
-- /newfed <name> - Create a new federation
-- /renamefed <id> <newname> - Rename your federation
-- /delfed <id> - Delete your federation
-- /fedtransfer <id> <newowner> - Transfer federation ownership
-    `,
-    user: `
-User Commands:
-- /joinfed <id> - Join a federation
-- /leavefed <id> - Leave a federation
-- /fedinfo <id> - Get information about a federation
-- /fedadmins <id> - List federation admins
-    `
+    main: 'Select a help topic:',
+    admin: 'Admin commands:\n/fpromote - Promote a member to admin\n/fdemote - Demote an admin to member',
+    owner: 'Owner commands:\n/newfed - Create a new federation\n/myfeds - List your federations',
+    user: 'User commands:\n/joinfed - Join a federation\n/leavefed - Leave a federation'
   }
 };
-
-module.exports = messages;
