8cc15bd074b816821effa5ed4ae468405fd3f998
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 20:46:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const { handleCommands, handleHelpCallback } = require('./commands');
-const { sequelize } = require('./db');
+const { connectDb } = require('./db');
+const { handleCommands } = require('./commands/index');
 
-// Initialize the bot with the token from the environment variables
-const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+require('dotenv').config();
 
-// Log database connection status
-sequelize.authenticate()
-  .then(() => {
-    console.log('Database connected.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, { polling: true });
 
-// Handle incoming messages
 bot.on('message', (msg) => {
-  if (msg.text) {
-    handleCommands(bot, msg);
+  handleCommands(bot, msg);
+});
+
+bot.on('callback_query', (callbackQuery) => {
+  const { message, data } = callbackQuery;
+  const chatId = message.chat.id;
+  
+  if (data === 'help') {
+    handleHelpCallback(bot, message);
+  } else if (data.startsWith('help_')) {
+    const section = data.split('_')[1];
+    handleHelpSectionCallback(bot, message, section);
   }
 });
 
-// Handle callback queries (e.g., from inline keyboards)
-bot.on('callback_query', (query) => {
-  handleHelpCallback(bot, query);
+connectDb().then(() => {
+  console.log('Bot is running...');
 });
-
-console.log('Bot is running...');
