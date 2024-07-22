b0a36f7186a426b2b53f07604dc570fa511641b9
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 21:28:02 +0200

    Delete messages.js

--- messages.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  start: 'Welcome to the Federation Bot! Use /help to see available commands.',
-  helpMenu: {
-    main: 'Select a help topic:',
-    admin: 'Admin commands:\n/fpromote - Promote a member to admin\n/fdemote - Demote an admin to member',
-    owner: 'Owner commands:\n/newfed - Create a new federation\n/myfeds - List your federations',
-    user: 'User commands:\n/joinfed - Join a federation\n/leavefed - Leave a federation'
-  }
-};
