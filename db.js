62347134f3a0badc09fb689ad38915c90a4d5085
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 20:56:42 +0200

    Update db.js

--- db.js
+++ db.js
@@ -1,111 +1,28 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  protocol: 'postgres',
   logging: false,
 });
 
-const User = sequelize.define('User', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+const Federation = require('./models/Federation')(sequelize);
+const User = require('./models/User')(sequelize);
 
-const Federation = sequelize.define('Federation', {
-  id: {
-    type: DataTypes.UUID,
-    primaryKey: true,
-    defaultValue: DataTypes.UUIDV4,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  ownerId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  logChannelId: {
-    type: DataTypes.INTEGER,
-    allowNull: true,
-  },
-});
-
-const Chat = sequelize.define('Chat', {
-  id: {
-    type: DataTypes.BIGINT,
-    primaryKey: true,
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  federationId: {
-    type: DataTypes.UUID,
-    allowNull: true,
-  },
-});
-
-const FederationAdmin = sequelize.define('FederationAdmin', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  federationId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-});
-
-const FederationBan = sequelize.define('FederationBan', {
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  federationId: {
-    type: DataTypes.UUID,
-    allowNull: false,
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-  reason: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
-
-const connectDb = async () => {
+async function connectDb() {
   try {
     await sequelize.authenticate();
     console.log('Database connected.');
     await sequelize.sync({ alter: true });
     console.log('Database & tables created!');
   } catch (error) {
-    console.error('Error connecting to the database:', error);
+    console.error('Unable to connect to the database:', error);
   }
-};
+}
 
 module.exports = {
   sequelize,
-  User,
   Federation,
-  Chat,
-  FederationAdmin,
-  FederationBan,
+  User,
   connectDb,
 };
