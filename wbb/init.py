fede83c7e5db729d408af010d45625365353e4ba
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 16:07:30 +0200

    Create init.py

---
+++ wbb/init.py
@@ -0,0 +1,10 @@
+import os
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+SUDOERS = [123456789]  # List of sudo user IDs
+BOT_ID = 123456789  # Your bot's user ID
+LOG_GROUP_ID = -100123456789  # Log group chat ID
+
