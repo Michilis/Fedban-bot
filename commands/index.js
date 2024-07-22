11821f4be03690c9ab5472f11ac334f4d244c3ca
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 20:51:22 +0200

    Update index.js

--- commands/index.js
+++ commands/index.js
@@ -1,50 +1,58 @@
-const federationCommands = require('./federation');
-const userCommands = require('./user');
-const adminCommands = require('./admin');
+const { handleStart, handleHelp } = require('./general');
+const { handleNewFed, handleMyFeds, handleJoinFed, handleLeaveFed, handleFedInfo, handleFedAdmins } = require('./federation');
+const { handleFPromote, handleFDemote, handleFBan, handleUnFBan } = require('./admin');
+const { handleHelpCallback, handleHelpSectionCallback } = require('./help');
 
-function handleCommands(bot, msg) {
-  const command = msg.text.split(' ')[0].toLowerCase();
-
-  switch (command) {
-    case '/start':
-      return userCommands.handleStart(bot, msg);
-    case '/help':
-      return userCommands.handleHelp(bot, msg);
-    case '/newfed':
-      return federationCommands.handleNewFed(bot, msg);
-    case '/delfed':
-      return federationCommands.handleDelFed(bot, msg);
-    case '/fedtransfer':
-      return federationCommands.handleFedTransfer(bot, msg);
-    case '/myfeds':
-      return federationCommands.handleMyFeds(bot, msg);
-    case '/renamefed':
-      return federationCommands.handleRenameFed(bot, msg);
-    case '/setfedlog':
-      return federationCommands.handleSetFedLog(bot, msg);
-    case '/unsetfedlog':
-      return federationCommands.handleUnsetFedLog(bot, msg);
-    case '/joinfed':
-      return federationCommands.handleJoinFed(bot, msg);
-    case '/leavefed':
-      return federationCommands.handleLeaveFed(bot, msg);
-    case '/fedinfo':
-      return federationCommands.handleFedInfo(bot, msg);
-    case '/fedadmins':
-      return federationCommands.handleFedAdmins(bot, msg);
-    case '/fpromote':
-      return adminCommands.handleFPromote(bot, msg);
-    case '/fdemote':
-      return adminCommands.handleFDemote(bot, msg);
-    case '/fban':
-      return adminCommands.handleFBan(bot, msg);
-    case '/unfban':
-      return adminCommands.handleUnFBan(bot, msg);
-    default:
-      return bot.sendMessage(msg.chat.id, 'Unknown command.');
+async function handleCommands(bot, msg) {
+  if (msg.text) {
+    const command = msg.text.split(' ')[0].toLowerCase();
+    
+    switch (command) {
+      case '/start':
+        await handleStart(bot, msg);
+        break;
+      case '/help':
+        await handleHelp(bot, msg);
+        break;
+      case '/newfed':
+        await handleNewFed(bot, msg);
+        break;
+      case '/myfeds':
+        await handleMyFeds(bot, msg);
+        break;
+      case '/joinfed':
+        await handleJoinFed(bot, msg);
+        break;
+      case '/leavefed':
+        await handleLeaveFed(bot, msg);
+        break;
+      case '/fedinfo':
+        await handleFedInfo(bot, msg);
+        break;
+      case '/fedadmins':
+        await handleFedAdmins(bot, msg);
+        break;
+      case '/fpromote':
+        await handleFPromote(bot, msg);
+        break;
+      case '/fdemote':
+        await handleFDemote(bot, msg);
+        break;
+      case '/fban':
+        await handleFBan(bot, msg);
+        break;
+      case '/unfban':
+        await handleUnFBan(bot, msg);
+        break;
+      default:
+        await bot.sendMessage(msg.chat.id, 'Unknown command. Use /help to see the available commands.');
+        break;
+    }
   }
 }
 
 module.exports = {
   handleCommands,
+  handleHelpCallback,
+  handleHelpSectionCallback,
 };
