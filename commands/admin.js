17a06e344d71defcb01eb5ba3e36a80e145e8c17
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 20:58:53 +0200

    Update admin.js

--- commands/admin.js
+++ commands/admin.js
@@ -1,24 +1,24 @@
-const { FederationAdmin, FederationBan, Chat } = require('../db');
+const { Federation } = require('../db');
 
 async function handleFPromote(bot, msg) {
-  // Implement the federation promote admin logic here
+  // Implement the logic for promoting an admin
 }
 
 async function handleFDemote(bot, msg) {
-  // Implement the federation demote admin logic here
+  // Implement the logic for demoting an admin
 }
 
 async function handleFBan(bot, msg) {
-  // Implement the federation ban logic here
+  // Implement the logic for banning a user
 }
 
 async function handleUnFBan(bot, msg) {
-  // Implement the federation unban logic here
+  // Implement the logic for unbanning a user
 }
 
 module.exports = {
   handleFPromote,
   handleFDemote,
   handleFBan,
-  handleUnFBan,
+  handleUnFBan
 };
