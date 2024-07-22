95df8600117fb9f397dff8227e29e9d74f03d11e
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-22 17:39:55 +0200

    Update init.py

--- wbb/init.py
+++ wbb/init.py
@@ -1,9 +1,7 @@
+from pyrogram import Client
 import os
-import logging
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
-
-BOT_ID = int(os.getenv("BOT_ID"))
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))
-USERBOT_PREFIX = os.getenv("USERBOT_PREFIX")
+app = Client(
+    "fedban_bot",
+    bot_token=os.getenv("BOT_TOKEN")
+)
