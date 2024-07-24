80a18dd434d50341b3298ea80942158abf141b25
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-24 20:40:24 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,20 @@
+from pyrogram import Client, filters
+from config import BOT_TOKEN
+from database.db import create_tables
+from handlers.help import register_help_handlers
+
+app = Client("my_bot", bot_token=BOT_TOKEN)
+
+@app.on_message(filters.command("start"))
+async def start_command(client, message):
+    await help_menu(client, message)
+
+@app.on_message(filters.command("fedhelp"))
+async def help_command(client, message):
+    await help_menu(client, message)
+
+create_tables()
+register_help_handlers(app)
+
+if __name__ == "__main__":
+    app.run()
