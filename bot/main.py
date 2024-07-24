fbb0c06c25b78b53a3935d6513d28525b38e64ac
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-24 20:27:46 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -8,13 +8,12 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '.')))
 
 from bot.app import app  # Import the app instance from bot.app
 from bot.db import init_db
-
-# Import commands and callbacks to register them
 import bot.commands
 import bot.callbacks
 
 async def main():
     await init_db()
+    bot.callbacks.register_help_handlers(app)
     await app.start()
     print("Bot is running...")
     await app.idle()
