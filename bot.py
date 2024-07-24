38f4a885751e3943b024c7a9b22586a0f0af5826
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-25 00:45:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,8 +2,12 @@ from pyrogram import Client, filters
 from config import BOT_TOKEN
 from database.db import create_tables
 from handlers.help import register_help_handlers
+from handlers.federation import *
 
-app = Client("my_bot", bot_token=BOT_TOKEN)
+app = Client(
+    "my_bot",
+    bot_token=BOT_TOKEN
+)
 
 @app.on_message(filters.command("start"))
 async def start_command(client, message):
