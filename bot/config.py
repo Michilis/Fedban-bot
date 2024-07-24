d9327b0cad75774dc1184bc4ca3892c716fe8de1
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-24 19:19:13 +0200

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+DATABASE_URL = os.getenv("DATABASE_URL")
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))
+SUDOERS = list(map(int, os.getenv("SUDOERS").split()))
