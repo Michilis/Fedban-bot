375f28b3a0465d5558f3592c58b18dfdede251ff
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-24 19:23:11 +0200

    Update utils.py

--- bot/utils.py
+++ bot/utils.py
@@ -10,4 +10,9 @@ def create_confirmation_markup(callback_data):
         [InlineKeyboardButton("Cancel", callback_data="cancel")]
     ])
 
+def extract_user_and_reason(message):
+    user_id = message.reply_to_message.from_user.id if message.reply_to_message else message.command[1]
+    reason = message.text.split(' ', 2)[2] if len(message.command) > 2 else None
+    return user_id, reason
+
 # Add more utility functions as needed
