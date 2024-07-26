9f66969d256b7a92ae2a87d108341808217d0132
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 04:39:26 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,30 +1,36 @@
 import logging
 import asyncio
-from telegram.ext import ApplicationBuilder
-from config import BOT_TOKEN, DEBUGGING
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+from config import TOKEN, DEBUGGING
 from bot.user_commands import register_user_command_handlers
 from bot.admin_commands import register_admin_command_handlers
 from bot.owner_commands import register_owner_command_handlers
-from bot.callbacks import register_callback_handlers
-from bot.db import init_db
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
 
 if DEBUGGING:
-    logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
 async def main():
-    await init_db()
-
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
-
+    app = ApplicationBuilder().token(TOKEN).build()
+    
     register_user_command_handlers(app)
     register_admin_command_handlers(app)
     register_owner_command_handlers(app)
-    register_callback_handlers(app)
+    
+    async with app:
+        logger.info("Bot is running...")
+        await app.start()
+        await app.updater.start_polling()
+        await app.stop()
+        await app.updater.stop()
 
-    print("Bot is running...")
-    await app.run_polling()
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
