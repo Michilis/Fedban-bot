20981d3b6e585a72fa87cae8df33ea736dd82578
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 03:31:17 +0200

    Update app.py

--- bot/app.py
+++ bot/app.py
@@ -1,4 +1,4 @@
 from pyrogram import Client
 from config import BOT_TOKEN
 
-app = Client("fedban_bot", bot_token=BOT_TOKEN)
+app = Client("fedban_bot", bot_token=BOT_TOKEN, api_id=None, api_hash=None)
