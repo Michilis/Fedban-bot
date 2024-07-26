1693dd73cec29316ef33ca3b700e324f6dc57a56
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 04:41:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Telegram bot token
-TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('BOT_TOKEN')
 
 # Debugging mode
 DEBUGGING = os.getenv('DEBUGGING', 'False').lower() in ['true', '1', 't']
 
-# Other configurations can go here
+# Log group ID
 LOG_GROUP_ID = int(os.getenv('LOG_GROUP_ID'))
+
+# Database URL
+DATABASE_URL = os.getenv('DATABASE_URL')
