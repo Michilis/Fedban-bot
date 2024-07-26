f25eff99b5ad6498a626217a168b08352877cba9
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 03:55:38 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,16 +1,19 @@
 import asyncio
-from bot.app import app
+from telegram.ext import ApplicationBuilder
 from bot.db import init_db
 from bot.commands import register_command_handlers
+from config import BOT_TOKEN
 
 async def main():
     await init_db()
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
     register_command_handlers(app)
     print("Bot is running...")
+    await app.initialize()
     await app.start()
     await app.updater.start_polling()
+    await app.stop()
+    await app.shutdown()
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    loop.run_forever()
+    asyncio.run(main())
