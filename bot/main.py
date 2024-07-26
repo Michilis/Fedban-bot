f36f3952c4772c2b00913f66550d9d2c525af7c9
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 03:57:50 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -12,4 +12,11 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except RuntimeError as e:
+        if str(e) == 'This event loop is already running':
+            loop = asyncio.get_running_loop()
+            loop.run_until_complete(main())
+        else:
+            raise
