420fddb55482f50168271f55ffbf9eed4c6baea5
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 04:15:52 +0200

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,26 @@
+import logging
 import asyncio
-import nest_asyncio
 from telegram.ext import ApplicationBuilder
-from bot.db import init_db
+from config import BOT_TOKEN, DEBUGGING
 from bot.commands import register_command_handlers
-from config import BOT_TOKEN
+
+if DEBUGGING:
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+else:
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 async def main():
-    await init_db()
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     register_command_handlers(app)
+
     print("Bot is running...")
     await app.run_polling()
 
-if __name__ == "__main__":
-    nest_asyncio.apply()
-    asyncio.run(main())
+if __name__ == '__main__':
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    finally:
+        loop.run_until_complete(main())
