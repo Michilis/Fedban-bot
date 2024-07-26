707d929a79b28ac10a3a380ad33d4e3423c43216
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 03:26:06 +0200

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,3 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Log group ID for logging actions
 LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))
-
-# List of user IDs who have sudo privileges
-SUDOERS = list(map(int, os.getenv("SUDOERS").split(',')))
