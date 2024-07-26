f32a83442845ddd5de212377685a7e580e8462b6
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 04:15:27 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,9 @@
-import os
 from dotenv import load_dotenv
+import os
 
-# Load environment variables from a .env file
 load_dotenv()
 
-# Telegram bot token
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-# PostgreSQL database URL
+LOG_GROUP_ID = os.getenv("LOG_GROUP_ID")
 DATABASE_URL = os.getenv("DATABASE_URL")
-
-# Log group ID for logging actions
-LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID"))
+DEBUGGING = os.getenv("DEBUGGING", "False").lower() in ("true", "1", "t")
