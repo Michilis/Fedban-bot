e77aadc6298f74982a6a8ad7ec52437026a3eece
Michilis <120072772+Michilis@users.noreply.github.com>
2024-07-26 04:32:03 +0200

    Update user_commands.py

--- bot/user_commands.py
+++ bot/user_commands.py
@@ -1,13 +1,14 @@
 import logging
 from telegram import Update
-from telegram.ext import CallbackContext
+from telegram.ext import CallbackContext, CommandHandler
 from bot.db import (
-    get_fed_info,
+    get_fed_id,
     chat_join_fed,
     chat_leave_fed,
     chat_id_and_names_in_fed,
 )
-from config import DEBUGGING, LOG_GROUP_ID, MESSAGES
+from config import DEBUGGING, LOG_GROUP_ID
+from messages import MESSAGES
 
 if DEBUGGING:
     logger = logging.getLogger()
@@ -16,19 +17,19 @@ if DEBUGGING:
 async def start(update: Update, context: CallbackContext) -> None:
     if DEBUGGING:
         logger.debug(f"User {update.message.from_user.id} issued /start command")
-    await update.message.reply_text(MESSAGES["start"])
+    await update.message.reply_text(MESSAGES["start_message"])
 
 async def chat_join_fed(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     fed_id = context.args[0]
     await chat_join_fed(fed_id, chat_id)
-    await update.message.reply_text(MESSAGES["chat_join_fed"])
+    await update.message.reply_text(MESSAGES["group_joined_federation"].format(fed_name=fed_id))
 
 async def chat_leave_fed(update: Update, context: CallbackContext) -> None:
     chat_id = update.message.chat_id
     fed_id = await get_fed_id(chat_id)
     await chat_leave_fed(fed_id, chat_id)
-    await update.message.reply_text(MESSAGES["chat_leave_fed"])
+    await update.message.reply_text(MESSAGES["group_left_federation"].format(fed_name=fed_id))
 
 async def chat_id_and_names_in_fed(update: Update, context: CallbackContext) -> None:
     fed_id = context.args[0]
